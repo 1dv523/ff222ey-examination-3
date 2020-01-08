2b26012a04d10e7bd81fa28ab85fd41f1c6064c3
Findlay <findlay12@ymail.com>
2020-01-08 22:09:21 +0100

    oath app

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(express.urlencoded({ extended: false }))
 
 app.get('/error', (req, res) => {
   console.log('lol')
-  res.send('The node envirment is ' + process.env.NODE_ENV)
+  res.send('The node envirment is ' + process.env.GITHUB_TOKEN)
   // process.exit(1)
 })
 
