6798e63569ca90703bb6c83df6bcc9f3d23ef48c
Findlay <findlay12@ymail.com>
2020-01-08 21:42:34 +0100

    oath app

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -22,7 +22,7 @@ homeController.callback = async (req, res, next) => {
     client_secret: process.env.client_secret,
     code: code
   }
-  res.send(body.code)
+  res.send(body.client_secret)
 
   // let response = await fetch('https://github.com/login/oauth/access_token', {
   //   method: 'post',
