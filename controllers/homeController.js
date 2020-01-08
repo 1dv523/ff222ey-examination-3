226e63738a2787501bb1356bfe732fac829d3900
Findlay <findlay12@ymail.com>
2020-01-08 21:20:59 +0100

    oath app

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -16,22 +16,22 @@ homeController.hooks = (req, res, next) => {
 }
 
 homeController.callback = async (req, res, next) => {
-  // const code = req.query.code
-  // const body = {
-  //   client_id: process.env.client_id,
-  //   client_secret: process.env.client_secret,
-  //   code: code
-  // }
-  // let response = await fetch('https://github.com/login/oauth/access_token', {
-  //   method: 'post',
-  //   body: JSON.stringify(body),
-  //   headers: { 'Content-Type': 'application/json', Accept: 'application/json' }
-  // })
-  // response = await response.json()
+  const code = req.query.code
+  const body = {
+    client_id: process.env.client_id,
+    client_secret: process.env.client_secret,
+    code: code
+  }
+  let response = await fetch('https://github.com/login/oauth/access_token', {
+    method: 'post',
+    body: JSON.stringify(body),
+    headers: { 'Content-Type': 'application/json', Accept: 'application/json' }
+  })
+  response = await response.json()
   res.send('lol')
 
 
-
+  
 
   // response = await response.json()
   // const token = response.access_token
